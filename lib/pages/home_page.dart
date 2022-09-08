3d89b6a17ef4928efad536aa737c34f2daa66f59
mathed1as <matheus.vex@gmail.com>
2022-09-08 16:57:58 -0300

    add dio dependence

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/controllers/home_controller.dart';
 import 'package:helloworld/models/post_model.dart';
-import 'package:helloworld/repositories/home_repository_mock.dart';
+import 'package:helloworld/repositories/home_repository_imp.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController _controller = HomeController(HomeRepositoryMock());
+  final HomeController _controller = HomeController(HomeRepositoryImp());
 
   @override
   void initState() {
@@ -21,18 +21,21 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     body: ValueListenableBuilder<List<PostModel>>(
-       valueListenable: _controller.posts,
-       builder: (_, list, __) {
-         return ListView.builder(
-           itemCount: list.length,
-          itemBuilder: (_, idx) => ListTile(
-            title: Text(list[idx].title)
-          ),
-        );
-       },
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: ValueListenableBuilder<List<PostModel>>(
+        valueListenable: _controller.posts,
+        builder: (_, list, __) {
+            return ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: list.length,
+              shrinkWrap: true,
+              itemBuilder: (_, idx) => ListTile(
+                title: Text(list[idx].title)
+              ),
+          );
+        },
       )
-   );
+    );
   }
 }
